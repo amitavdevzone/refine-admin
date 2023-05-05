e1cb6e249df7c41313e5ce1128234577239e3892
Amitav Roy <reachme@amitavroy.com>
2023-05-05 06:10:10 +0530

    Adding filter, sort and search to the table

--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,14 +1,45 @@
-import { useTable } from "@refinedev/antd";
-import { List, Table } from "antd";
+import { FilterDropdown, SaveButton, useTable } from "@refinedev/antd";
+import { Form, Input, List, Radio, Table } from "antd";
+import { IPost } from "../../interfaces";
+import { HttpError } from "@refinedev/core";
+
+interface ISearch {
+  title: string;
+}
 
 const PostsList = () => {
-  const { tableProps } = useTable();
+  const { tableProps, searchFormProps } = useTable<IPost, HttpError, ISearch>({
+    sorters: {
+      initial: [{ field: "id", order: "desc" }],
+    },
+    onSearch: (values) => {
+      return [{ field: "title", operator: "contains", value: values.title }];
+    },
+  });
   return (
     <List>
+      <Form {...searchFormProps} layout="inline">
+        <Form.Item name="title">
+          <Input placeholder="Search by title" />
+        </Form.Item>
+        <SaveButton onClick={searchFormProps.form?.submit} />
+      </Form>
       <Table {...tableProps} rowKey="id">
         <Table.Column dataIndex="id" title="ID" />
         <Table.Column dataIndex="title" title="Post title" />
-        <Table.Column dataIndex="status" title="Status" />
+        <Table.Column
+          dataIndex="status"
+          title="Status"
+          filterDropdown={(props) => (
+            <FilterDropdown {...props}>
+              <Radio.Group>
+                <Radio value="published">Published</Radio>
+                <Radio value="draft">Draft</Radio>
+                <Radio value="rejected">Rejected</Radio>
+              </Radio.Group>
+            </FilterDropdown>
+          )}
+        />
       </Table>
     </List>
   );
